cbdfecb0f374eb61224ccb12eef09f2f65cbeafb
Henry Rochester <henryrochester8@gmail.com>
2026-03-15 13:02:53 +0200

    simulator v1 with solve_IVP

---
+++ src/quadrotor/simulator.py
@@ -0,0 +1,21 @@
+'''
+In this feel, we shall implement the simulator to check that our drone dynamics behave as we expect. 
+The way in which we confirm this is by intergrating our 12 ODEs forward in time, over a time horizon and check that the output is as we expected. 
+'''
+
+import numpy as np
+from scipy.integrate import solve_ivp
+from quadrotor.dynamics import f 
+from quadrotor.params import Quadrotorparams
+
+
+def simulation(x_0: np.array, u: np.array, t_span: tuple, params:Quadrotorparams, t_eval: np.array = None):
+
+    sol = solve_ivp(
+        lambda t, x: f(x,u,params),
+        t_span,
+        x_0,
+        method='RK45',
+        t_eval=t_eval
+    )
+    return sol 
