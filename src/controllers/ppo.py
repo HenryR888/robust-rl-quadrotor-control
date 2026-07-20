d3433e500b0715e761f1fc4d2280eadcd62fae3b
Henry Rochester <henryrochester8@gmail.com>
2026-07-20 21:07:27 +0200

    update new way of choosing policy based on rew-p-s

--- src/controllers/ppo.py
+++ src/controllers/ppo.py
@@ -9,8 +9,9 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.callbacks import EvalCallback, BaseCallback
 from stable_baselines3.common.env_checker import check_env
-from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
+from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize, sync_envs_normalization
 from stable_baselines3.common.running_mean_std import RunningMeanStd
+from stable_baselines3.common.evaluation import evaluate_policy
 
 from envs.hover_env import HoverEnv
 
@@ -37,6 +38,102 @@ class SaveNormalizeCallback(BaseCallback):
     def _on_step(self) -> bool:
         self.vec_normalize_env.save(self.save_path)
         return True
+    
+class RateEvalCallback(EvalCallback):
+      '''
+      Here we change the way in which the policy is checkpointed and chosen. Instead of based on total reward, we choose the policy based on best reward-per-step (total reward/total timesteps alive)
+      '''
+
+      def __init__(self, *args, **kwargs):
+          super().__init__(*args, **kwargs)
+          self.best_reward_rate = -np.inf
+
+      def _on_step(self) -> bool:
+          continue_training = True
+
+          if self.eval_freq > 0 and self.n_calls % self.eval_freq == 0:
+              if self.model.get_vec_normalize_env() is not None:
+                  try:
+                      sync_envs_normalization(self.training_env, self.eval_env)
+                  except AttributeError as e:
+                      raise AssertionError(
+                          "check the eval callback in the SB3 code"
+                      ) from e
+
+              self._is_success_buffer = []
+
+              episode_rewards, episode_lengths = evaluate_policy(
+                  self.model,
+                  self.eval_env,
+                  n_eval_episodes=self.n_eval_episodes,
+                  render=self.render,
+                  deterministic=self.deterministic,
+                  return_episode_rewards=True,
+                  warn=self.warn,
+                  callback=self._log_success_callback,
+              )
+
+              if self.log_path is not None:
+                  self.evaluations_timesteps.append(self.num_timesteps)
+                  self.evaluations_results.append(episode_rewards)
+                  self.evaluations_length.append(episode_lengths)
+
+                  kwargs = {}
+                  if len(self._is_success_buffer) > 0:
+                      self.evaluations_successes.append(self._is_success_buffer)
+                      kwargs = dict(successes=self.evaluations_successes)
+
+                  np.savez(
+                      self.log_path,
+                      timesteps=self.evaluations_timesteps,
+                      results=self.evaluations_results,
+                      ep_lengths=self.evaluations_length,
+                      **kwargs,
+                  )
+
+              mean_reward, std_reward = np.mean(episode_rewards), np.std(episode_rewards)
+              mean_ep_length, std_ep_length = np.mean(episode_lengths), np.std(episode_lengths)
+              self.last_mean_reward = float(mean_reward)
+
+              # reward per step, averaged across eval episodes will be our new selection metric:
+              episode_rates = np.array(episode_rewards) / np.array(episode_lengths)
+              mean_reward_rate = float(np.mean(episode_rates))
+
+              if self.verbose >= 1:
+                  print(f"Eval num_timesteps={self.num_timesteps}, episode_reward={mean_reward:.2f} +/- {std_reward:.2f}")
+                  print(f"Episode length: {mean_ep_length:.2f} +/- {std_ep_length:.2f}")
+                  print(f"Reward rate: {mean_reward_rate:.3f}/step")
+
+              self.logger.record("eval/mean_reward", float(mean_reward))
+              self.logger.record("eval/mean_ep_length", mean_ep_length)
+              self.logger.record("eval/mean_reward_rate", mean_reward_rate)
+
+              if len(self._is_success_buffer) > 0:
+                  success_rate = np.mean(self._is_success_buffer)
+                  if self.verbose >= 1:
+                      print(f"Success rate: {100 * success_rate:.2f}%")
+                  self.logger.record("eval/success_rate", success_rate)
+
+              self.logger.record("time/total_timesteps", self.num_timesteps, exclude="tensorboard")
+              self.logger.dump(self.num_timesteps)
+
+              # for logging: 
+              if mean_reward > self.best_mean_reward:
+                  self.best_mean_reward = float(mean_reward)
+
+              if mean_reward_rate > self.best_reward_rate:
+                  if self.verbose >= 1:
+                      print("New best reward rate!")
+                  if self.best_model_save_path is not None:
+                      self.model.save(os.path.join(self.best_model_save_path, "best_model"))
+                  self.best_reward_rate = mean_reward_rate
+                  if self.callback_on_new_best is not None:
+                      continue_training = self.callback_on_new_best.on_step()
+
+              if self.callback is not None:
+                  continue_training = continue_training and self._on_event()
+
+          return continue_training
 
 def train_ppo(total_timesteps: int = 20_000_000, n_envs: int =4):
 
