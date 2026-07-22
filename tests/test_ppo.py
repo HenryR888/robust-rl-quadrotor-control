80eda4a7a357444bbf90e1ea8d3f04deb04cec17
Henry Rochester <henryrochester8@gmail.com>
2026-07-22 21:14:06 +0200

    add phase5 training function to commence phase5

--- tests/test_ppo.py
+++ tests/test_ppo.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 from stable_baselines3.common.env_checker import check_env
 
 from envs.hover_env import HoverEnv
-from controllers.ppo import train_ppo, train_ppo_curriculum, train_ppo_curriculum_from_phase3, train_ppo_curriculum_from_phase4, PPOController, RelativeObsWrapper, MODEL_DIR
+from controllers.ppo import train_ppo, train_ppo_curriculum, train_ppo_curriculum_from_phase3, train_ppo_curriculum_from_phase4, train_ppo_curriculum_from_phase5, PPOController, RelativeObsWrapper, MODEL_DIR
 
 
 print("=== 1. Gymnasium env. Check ===")
@@ -28,7 +28,8 @@ print("RelativeObsWrapper passed.\n")
 #train_ppo(total_timesteps=20_000_000, n_envs=4)
 #train_ppo_curriculum(phase2_timesteps=20_000_000, phase3_timesteps=20_000_000, phase4_timesteps=20_000_000, phase5_timesteps=20_000_000, n_envs=4)
 #train_ppo_curriculum_from_phase3(phase3_timesteps=20_000_000, phase4_timesteps=20_000_000, phase5_timesteps=20_000_000, n_envs=4)
-train_ppo_curriculum_from_phase4(phase4_timesteps=20_000_000, phase5_timesteps=20_000_000, n_envs=4)
+#train_ppo_curriculum_from_phase4(phase4_timesteps=20_000_000, phase5_timesteps=20_000_000, n_envs=4)
+train_ppo_curriculum_from_phase5(phase5_timesteps=20_000_000, n_envs=4)
 
 
 print("=== 2. Phase 1 Evaluation (no wind, near target) ===")
